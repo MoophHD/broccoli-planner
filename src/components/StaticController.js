c266e08fd4bd52dee17646e76c24425a2db59113
MoophHD <d_sizykh@mail.ru>
2017-11-15 10:27:00 +0300

    stylefix

--- src/components/StaticController.js
+++ src/components/StaticController.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function StaticController(props) {
     return (
-    <div style={{display:props.isActive ? "block" : "none"}} onClick={props.handleClick} className="ctrInput">
+    <div style={{display:props.isActive ? "block" : "none"}} onClick={props.handleClick} className="inputCore ctrInput">
         {props.chuncks.map((chunck, ind) => {
-            return(<div key={ind} className="inputCore staticChunck" data-order={ind}>{chunck}</div>)
+            return(<div key={ind} className="staticChunck" data-order={ind}>{chunck}</div>)
         })}
     </div>)
 }
